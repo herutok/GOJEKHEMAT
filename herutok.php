f5f4d807df028168be48af5527a75bff9360fd6f
herutok <ureh.14@gmail.com>
2020-03-05 07:19:13 +0700

    Update herutok.php

--- herutok.php
+++ herutok.php
@@ -35,7 +35,7 @@ function change(){
         echo color("yellow",".");
         sleep(1);
         }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"GOFOOD021120A"}');
+        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"G-MPW4WBM"}');
         $message = fetch_value($code1,'"message":"','"');
         if(strpos($code1, 'Promo kamu sudah bisa dipakai')){
         echo "\n".color("green","+] Message: ".$message);
