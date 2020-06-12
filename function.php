fe7c6ace6187414616350f510a70b0b10b17a115
herutok <ureh.14@gmail.com>
2020-06-12 10:16:13 +0700

    Update function.php

--- function.php
+++ function.php
@@ -9,6 +9,7 @@ $header[] = "X-AppVersion: 3.40.2";
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: id_ID";
+$header[] = "X-Location: -7.5754884201,110".mt_rand(82000000,89000000);
 if ($pin):
 $header[] = "pin: $pin";
     endif;
